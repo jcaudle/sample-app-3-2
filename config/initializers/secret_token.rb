697d5bb6dce844855fa908cab70b4d58eb4d14ec
Joseph Caudle <joseph.caudle@gmail.com>
2012-05-05 14:01:31 -0500

    Initialize rails framework & put Gemfile together

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp32::Application.config.secret_token = '0f763bc0797f06173f5e8349a66821eb16ff1dd120fe591200e61b5b81b52587674a2c29611bbd1e588b8592f5fe02af044512556ee6bb0335527b2e67767136'
