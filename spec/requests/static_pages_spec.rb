27f058f08b8a1f1573e5a1a994a51a1c3bdef283
Joseph Caudle <joseph.caudle@gmail.com>
2012-05-20 08:18:50 -0500

    Migrated to RSpec let function

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-  title_text = "Ruby on Rails Tutorial Sample App | "
+  let(:base_title)  { "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
@@ -12,7 +12,7 @@ describe "Static Pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => title_text + "Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -25,7 +25,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => title_text + "Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -38,7 +38,7 @@ describe "Static Pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => title_text + "About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
@@ -51,7 +51,7 @@ describe "Static Pages" do
 
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text => title_text + 'Contact Us')
+      page.should have_selector('title', :text => "#{base_title} | Contact Us")
     end
   end
 end
