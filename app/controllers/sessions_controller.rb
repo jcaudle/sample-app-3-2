0fbb0a350a2ffd18bea5e6ec5f5b85d4dc809642
Joseph Caudle <joseph.caudle@gmail.com>
2012-05-31 14:46:11 -0500

    Added error flash for invalid signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
 	end
 	
 	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			# Sign in and redirect to user show page
+		else
+			# flash an error message and re-render the signin form
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 	
 	def destroy
