6cb3ed23b8f09a947660f7a40382424005d698b8
Joseph Caudle <joseph.caudle@gmail.com>
2012-05-31 17:06:40 -0500

    Added remember_token for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by_email(params[:session][:email])
 		if user && user.authenticate(params[:session][:password])
-			# Sign in and redirect to user show page
+			sign_in user
+			redirect_to user
 		else
-			# flash an error message and re-render the signin form
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
 		end
